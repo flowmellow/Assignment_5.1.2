137570b8e566945e74990c64535818c497a711db
flowmellow <michael.floresmmt@gmail.com>
2024-06-06 11:11:22 -0400

    Add project files.

---
+++ Assignment_5.1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+Test Data :
+Enter a number: 1234
+Expected Output :
+The sum of the digits of the number 1234 is : 10
+*/
+
+namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1234;
+            int sum = SumOfDigits(number);
+                   
+            Console.WriteLine($"The sum of number {number} is: {sum}");
+                        
+        }
+
+        static int SumOfDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+
+        //static int SumOfDigits(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 0;
+        //    }
+            
+        //    return (number % 10) + SumOfDigits(number / 10);
+        //}
+    }
+}
